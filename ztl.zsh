4f6c75676dc4bec0fc59730cea31384bae1c3ce8
nnao45 <n4sekai5y@gmail.com>
2018-09-14 19:00:22 +0900

    Create ztl.zsh

---
+++ ztl.zsh
@@ -0,0 +1,55 @@
+#!/usr/local/bin/zsh
+
+ANSIFILTER=''
+
+if which ansifilter > /dev/null 2>&1; then
+  ANSIFILTER='ansifilter'
+elif which ~/.ztl/ansifilter > /dev/null 2>&1; then
+  ANSIFILTER='~/.ztl-dir/ansifilter'
+else
+  while true; do
+    echo "ansifilter not found. Do you want to install ansifilter?(y/n)"
+    read answer
+    case $answer in
+      y)
+        PWD=`pwd`
+        mkdir ~/.ztl
+        curl http://www.andre-simon.de/zip/ansifilter-2.1.tar.bz --output /tmp/ansifilter-2.1.tar.bz2
+        tar -jxvf /tmp/ansifilter-2.1.tar.bz2 -C /tmp/
+        cd /tmp/ansifilter-2.1
+        make
+        mv src/ansifilter ~/.ztl/ansifilter
+        rm -rf /tmp/ansifilter-2.1*
+        ANSIFILTER='~/.ztl-dir/ansifilter'
+        cd ${PWD}
+        ;;
+      n)
+        echo "Bye."
+        exit 1
+        ;;
+      *)
+        echo "Please type the y or n"
+        ;;
+    esac
+  done
+fi
+
+if [[ $TERM = screen ]] || [[ $TERM = screen-256color ]] ; then
+  local LOGDIR=$HOME/Documents/term_logs
+  local LOGFILE=$(date +${2}_%Y-%m-%d_%H-%M-%S.log)
+  local FILECOUNT=0
+  local MAXFILECOUNT=200
+  # 本スクリプト起動時に自動で$MAXFILECOUNTのファイル数以上ログファイルあれば消す
+  for file in `\find "$LOGDIR" -maxdepth 1 -type f -name "*.log" | sort --reverse`; do
+    FILECOUNT=`expr $FILECOUNT + 1`
+    if [ $FILECOUNT -ge $MAXFILECOUNT ]; then
+      rm -f $file
+    fi
+  done
+  [ ! -d $LOGDIR ] && mkdir -p $LOGDIR
+  tmux  set-option default-terminal "screen" \; \
+  pipe-pane        "cat - | ${ANSIFILTER} | ~/.zsh_timestamp >> $LOGDIR/$LOGFILE" \; \
+  display-message  "💾Started logging to $LOGDIR/$LOGFILE"
+  ${@} -o ConnectTimeout=5
+  tmux pipe-pane\; display-message  "🔔End logging to $LOGDIR/$LOGFILE"
+fi
