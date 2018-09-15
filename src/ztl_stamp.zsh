5f70dab7e646f6cabb7f3b395ef7eb249b2fd645
nnao45 <n4sekai5y@gmail.com>
2018-09-15 16:29:00 +0900

    self make perl

--- src/ztl_stamp.zsh
+++ src/ztl_stamp.zsh
@@ -3,5 +3,3 @@
 ZTL_WORKDIR=`dirname $0`
 
 ${ZTL_WORKDIR}/ts.perl '[%Y-%m-%d %H:%M:%.S]'
-#${ZTL_WORKDIR}/ztl.perl 
-#perl -M'POSIX' -nle 'use Time::HiRes qw(time); use POSIX qw(strftime); s/\s+$//; my $t = time; my $date = strftime "%Y%m%d %H:%M:%S", localtime $t; $date .= sprintf ".%03d", ($t-int($t))*1000; print "[$date] " .$_'
